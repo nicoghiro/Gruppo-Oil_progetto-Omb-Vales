1ba0f4872865e02036bfe94de7e3216b4d16065d
nicoghiro <nicolasghirardi251005@gmail.com>
2023-02-18 15:05:55 +0100

    programma arduino lettura dati
    in caso non si riesca ad aprire con applicativo arduino usare blocco note , selezionare com5 come porta seriale all'avvio

---
+++ Informatica/Gene/leggi_Seriale/leggi_Seriale.ino
@@ -0,0 +1,24 @@
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+   int sensorValue1 = analogRead(A1);
+ // print out the value you read:
+  Serial.print(sensorValue);
+  Serial.print(";");
+  Serial.println(sensorValue1);
+  delay(1);  
+  
+}
+
+
+
+
+
+ 
