8350bb3c906439a57920c7d88f596a66054d2904
nicoghiro <nicolasghirardi251005@gmail.com>
2024-04-10 11:33:22 +0200

    oh mamma
    wawawa

---
+++ Informatica/GruppoOilPrototipo/view/WebMenager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GruppoOilPrototipo.view
+{
+    public static class WebMenager
+    {
+       static string nome;
+       static string 
+
+    }
+}
